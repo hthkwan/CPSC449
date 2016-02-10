1546df15e0c62cc96a7c5836b06249536a025ace
Cory Hutchison <cory.hutchison@protonmail.ch>
2016-02-10 12:01:32 -0700

    Fixed typo in greedy.

--- ApocStrategyGreedy.hs
+++ ApocStrategyGreedy.hs
@@ -1,4 +1,4 @@
-module ApocStrategyGreed where
+module ApocStrategyGreedy where
 
 import Control.Monad.Trans.State.Lazy
 import Data.Maybe (fromJust, isNothing)
@@ -9,12 +9,12 @@ import ApocTools
 {- | This is just a placeholder for the human strategy: it always chooses to play
      (0,0) to (2,1).
 -}
-greed    :: Chooser
-greed b Normal        c = do
+greedy    :: Chooser
+greedy b Normal        c = do
     line <- getLine
     let vals = convertToIntList line
     return (Just [((vals !! 0),(vals !! 1)),((vals !! 2),(vals !! 3))])
-greed b PawnPlacement c = return (Just [(2,2)])
+greedy b PawnPlacement c = return (Just [(2,2)])
 
 {- | Converts the input line to a list of Ints.
 -}
