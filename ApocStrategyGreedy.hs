406a0e8333ac9cd78dfaf073ce35bc72c6bd0dae
Cory Hutchison <cory.hutchison@protonmail.ch>
2016-02-21 16:50:33 -0700

    Replaced tabs with spaces in ApocStrategyGreedy

--- ApocStrategyGreedy.hs
+++ ApocStrategyGreedy.hs
@@ -19,27 +19,27 @@ greedyMove :: Player -> [[Cell]] -> [(Int, Int)]
 greedyMove player board = getTheRightInput(getGreedyMove board (getAllMoves (getAllPieces player board) player board))
 
 
-getAllPieces 					:: Player -> [[Cell]] -> [(Int, Int)]
+getAllPieces    :: Player -> [[Cell]] -> [(Int, Int)]
 getAllPieces player board 
-			| player == White 	= [(x,y) | x <- [0..4], y <- [0..4], (getFromBoard board (x,y) == WK || getFromBoard board (x,y) == WP)]
-			| otherwise 		= [(x,y) | x <- [0..4], y <- [0..4], (getFromBoard board (x,y) == BK || getFromBoard board (x,y) == BP)]
-		
-getAllMoves 							:: [(Int, Int)] -> Player -> [[Cell]] -> [((Int, Int),(Int, Int))]
+    | player == White   = [(x,y) | x <- [0..4], y <- [0..4], (getFromBoard board (x,y) == WK || getFromBoard board (x,y) == WP)]
+    | otherwise         = [(x,y) | x <- [0..4], y <- [0..4], (getFromBoard board (x,y) == BK || getFromBoard board (x,y) == BP)]
+
+getAllMoves     :: [(Int, Int)] -> Player -> [[Cell]] -> [((Int, Int),(Int, Int))]
 getAllMoves ((x,y):xs) player board 
-			| getFromBoard board (x,y) == WK 	= [((x,y),(w,v)) | w <- [x-2..x+2], v <- [y-2..y+2]]
-			| getFromBoard board (x,y) == WP 	= [((x,y),(w,v)) | w <- [x], v <- [y+1]]
-			| getFromBoard board (x,y) == BK 	= [((x,y),(w,v)) | w <- [x-2..x+2], v <- [y-2..y+2]]
-			| getFromBoard board (x,y) == BP 	= [((x,y),(w,v)) | w <- [x], v <- [y-1]]
-			| otherwise 						= [((x,y),(w,v)) | w <- [x], v <- [y]]
-	
-getGreedyMove :: [[Cell]] -> [((Int,Int),(Int,Int))] -> ((Int, Int),(Int,Int))
+    | getFromBoard board (x,y) == WK    = [((x,y),(w,v)) | w <- [x-2..x+2], v <- [y-2..y+2]]
+    | getFromBoard board (x,y) == WP    = [((x,y),(w,v)) | w <- [x], v <- [y+1]]
+    | getFromBoard board (x,y) == BK    = [((x,y),(w,v)) | w <- [x-2..x+2], v <- [y-2..y+2]]
+    | getFromBoard board (x,y) == BP    = [((x,y),(w,v)) | w <- [x], v <- [y-1]]
+    | otherwise                         = [((x,y),(w,v)) | w <- [x], v <- [y]]
+
+getGreedyMove   :: [[Cell]] -> [((Int,Int),(Int,Int))] -> ((Int, Int),(Int,Int))
 getGreedyMove board (((x,y),(w,v)):xs)
-		| getFromBoard board (x,y) == WK && getFromBoard board (w,v) == BP		= ((x,y),(w,v))
-		| getFromBoard board (x,y) == BK && getFromBoard board (w,v) == WP 		= ((x,y),(w,v))
-		| getFromBoard board (x,y) == WP && getFromBoard board (w,v) == BP		= ((x,y),(w,v))
-		| getFromBoard board (x,y) == BP && getFromBoard board (w,v) == WP		= ((x,y),(w,v))
-		| getFromBoard board (w,v) == E 										= ((x,y),(w,v))
-		| otherwise																= getGreedyMove board xs
+    | getFromBoard board (x,y) == WK && getFromBoard board (w,v) == BP      = ((x,y),(w,v))
+    | getFromBoard board (x,y) == BK && getFromBoard board (w,v) == WP      = ((x,y),(w,v))
+    | getFromBoard board (x,y) == WP && getFromBoard board (w,v) == BP      = ((x,y),(w,v))
+    | getFromBoard board (x,y) == BP && getFromBoard board (w,v) == WP      = ((x,y),(w,v))
+    | getFromBoard board (w,v) == E                                         = ((x,y),(w,v))
+    | otherwise                                                             = getGreedyMove board xs
 
 getTheRightInput :: ((Int,Int),(Int,Int)) -> [(Int,Int)]
 getTheRightInput ((x,y),(w,v)) = [(x,y),(w,v)]
